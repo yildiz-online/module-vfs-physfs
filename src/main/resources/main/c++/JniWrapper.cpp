90709838136f537859d753288b45a0692becb314
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2019-01-12 12:52:46 +0100

    [YE-0] Simplify code.

--- src/main/resources/main/c++/JniWrapper.cpp
+++ src/main/resources/main/c++/JniWrapper.cpp
@@ -42,8 +42,9 @@ JNIEXPORT jlong JNICALL Java_jni_PhysFsWrapperNative_registerContainer(JNIEnv* e
     try {
         yz::physfs::Wrapper* wrapper = reinterpret_cast<yz::physfs::Wrapper*>(pointer);
         JniStringWrapper path = JniStringWrapper(env, jpath);
-        yz::physfs::Container* container = wrapper->registerContainer(path.getValue());
-        return reinterpret_cast<jlong>(container);
+        return -1L;
+        //yz::physfs::Container* container = wrapper->registerContainer(path.getValue());
+        //return reinterpret_cast<jlong>(container);
     } catch (std::exception& e) {
         throwException(env, e.what());
     }
