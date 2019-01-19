979165d45057742755f31af91b3e6ba7ee1821d1
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2019-01-19 23:59:35 +0100

    [TASK] Update parent.

--- deploy-maven-central.sh
+++ deploy-maven-central.sh
@@ -9,6 +9,7 @@ if [ "$BRANCH" = "develop" ]; then
 elif [ "$BRANCH" = "master" ]; then
   echo "Building release"
   openssl aes-256-cbc -pass pass:$OPENSSL_PWD -in private-key.gpg.enc -out private-key.gpg -d && gpg --import --batch private-key.gpg && mvn -V -s settings.xml clean deploy
+  mvn -V -s settings.xml deploy -Dmaven.plugin.nexus.skip
 else
   echo "Building unhandled branch"
   mvn -V -s settings.xml clean package
