3dfa1c7bd7d4ecf07fd3e0aa81f10d2d33633c87
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2019-03-04 17:20:19 +0100

    [WIP] Use common build files.

--- deploy-maven-central.sh
+++ deploy-maven-central.sh
@@ -5,12 +5,12 @@ echo "Building $BRANCH branch"
 if [ "$BRANCH" = "develop" ]; then
   echo "Building development version"
   openssl version -a
-  openssl aes-256-cbc -pass pass:$OPENSSL_PWD -in private-key.gpg.enc -out private-key.gpg -d && gpg --import --batch private-key.gpg && mvn -V -s settings.xml org.jacoco:jacoco-maven-plugin:prepare-agent clean deploy sonar:sonar -Dsonar.host.url=https://sonarcloud.io -Dsonar.organization=$SONAR_ORGANIZATION -Dsonar.login=$SONAR
+  openssl aes-256-cbc -pass pass:$OPENSSL_PWD -in ../build-resources/private-key.gpg.enc -out private-key.gpg -d && gpg --import --batch private-key.gpg && mvn -V -s ../build-resources/settings.xml org.jacoco:jacoco-maven-plugin:prepare-agent clean deploy sonar:sonar -Dsonar.host.url=https://sonarcloud.io -Dsonar.organization=$SONAR_ORGANIZATION -Dsonar.login=$SONAR
 elif [ "$BRANCH" = "master" ]; then
   echo "Building release"
-  openssl aes-256-cbc -pass pass:$OPENSSL_PWD -in private-key.gpg.enc -out private-key.gpg -d && gpg --import --batch private-key.gpg && mvn -V -s settings.xml clean deploy
-  mvn -V -s settings.xml deploy -Dmaven.plugin.nexus.skip
+  openssl aes-256-cbc -pass pass:$OPENSSL_PWD -in ../build-resources/private-key.gpg.enc -out private-key.gpg -d && gpg --import --batch private-key.gpg && mvn -V -s ../build-resources/settings.xml clean deploy
+  mvn -V -s ../build-resources/settings.xml deploy -Dmaven.plugin.nexus.skip
 else
   echo "Building unhandled branch"
-  mvn -V -s settings.xml clean package
+  mvn -V -s ../build-resources/settings.xml clean package
 fi
