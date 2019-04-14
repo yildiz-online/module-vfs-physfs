feb9ff5b79397f9aaad45ee10e72a287da3e104c
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2019-04-14 19:25:27 +0200

    [WIP] Add factory.

---
+++ src/main/java/be/yildizgames/module/vfs/VfsFactory.java
@@ -0,0 +1,14 @@
+package be.yildizgames.module.vfs;
+
+import be.yildizgames.common.libloader.NativeResourceLoader;
+import be.yildizgames.module.vfs.physfs.PhysFsWrapper;
+
+/**
+ * @author Grégory Van den Borre
+ */
+public class VfsFactory {
+
+    public static Vfs getVfs(NativeResourceLoader loader) {
+        return PhysFsWrapper.create(loader);
+    }
+}
